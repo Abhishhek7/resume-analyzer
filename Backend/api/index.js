08b32de883494206435f456656b97eb297ff44c6
Abhishhek7 <am1820639@gmail.com>
2026-08-15 20:38:00 +0530

    fix vercel express api routes

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,3 +1,27 @@
+// require("dotenv").config()
+
+// const app = require("../src/app")
+// const connectToDB = require("../src/config/database")
+
+// let dbPromise
+
+// module.exports = async (req, res) => {
+//     try {
+//         if (!dbPromise) {
+//             dbPromise = connectToDB()
+//         }
+
+//         await dbPromise
+
+//         return app(req, res)
+//     } catch (error) {
+//         console.error("Vercel server error:", error)
+//         return res.status(500).json({
+//             message: "Internal server error"
+//         })
+//     }
+// }
+
 require("dotenv").config()
 
 const app = require("../src/app")
@@ -13,9 +37,14 @@ module.exports = async (req, res) => {
 
         await dbPromise
 
+        if (req.url !== "/" && !req.url.startsWith("/api")) {
+            req.url = `/api${req.url}`
+        }
+
         return app(req, res)
     } catch (error) {
         console.error("Vercel server error:", error)
+
         return res.status(500).json({
             message: "Internal server error"
         })
